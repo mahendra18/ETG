9fd0bc27e159345b13ad2b5521da4824fa8311d1
mahendra18 <mahendragawade18@gmail.com>
2018-02-25 11:36:14 +0530

    Create datatablejsforvendor.js

---
+++ datatablejsforvendor.js
@@ -0,0 +1,56 @@
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+//|||||||||||||||***************DATATABLE FOR SELECT PURCHASE REQUISITION********************|||||||||||||||||\\
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
+				$(document).ready(function() {
+		            $('#view_enquiry_requisition').DataTable( {
+		                dom: 'lfrtip',
+		                /*"scrollX": true,*/
+		                select: true
+		            } );
+		        } );
+				
+				$(document).ready(function() {
+		            $('#view_enquiry_requisition1').DataTable( {
+		                dom: 'lfrtip',
+		                /*"scrollX": true,*/
+		                select: true
+		            } );
+		        } );
+				
+				$(document).ready(function() {
+		            $('#view_enquiry_requisition2').DataTable( {
+		                dom: 'lfrtip',
+		                /*"scrollX": true,*/
+		                select: true
+		            } );
+		        } );
+				
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+//|||||||||||||||***************DATATABLE FOR SELECT PURCHASE REQUISITION********************|||||||||||||||||\\
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
+	$(document).ready(function() {
+		$('#view_purchase_requisition').DataTable( {
+			dom: 'lfrtip',
+			select: true
+		} );
+	} );
+	
+	$(document).ready(function() {
+		$('#vendor_view_terms').DataTable( {
+			dom: 'lfrtip',
+			select: true
+		} );
+	} );
+	//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+	//|||||||||||||||||***************DATATABLE FOR VIEW PURCHASE REQUISITION********************|||||||||||||||||\\
+	//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+
+		$(document).ready(function() {
+			$('#admin_view_quotation').DataTable( {
+				dom: 'frt',
+				select: true
+			} );
+		} );
+		
